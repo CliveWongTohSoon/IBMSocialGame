6178ddd8c0a1d104817c92267cfe601983d46fd0
DHARSHANA LAKSHMANAN <dharshana@dyn3143-193.wlan.ic.ac.uk>
2018-05-10 14:15:49 +0100

    Colour change

--- assets/app/game/game.component.ts
+++ assets/app/game/game.component.ts
@@ -38,18 +38,12 @@ export class GameComponent {
        // randomDir();
         console.log(numberOfPlayers);
 
-<<<<<<< HEAD
-        let dir2 = this.gameService.randomDir();
-        let x2 = this.gameService.randomCoor();
-        let y2 = this.gameService.randomCoor();
-       this.battleFieldP2 = this.gameService.createGame(this.battleField, x2, y2,'#003399','#007399',dir2[0], dir2[1], dir2[2], dir2[3]);
-=======
         this.allBattleShip = this.gameService.createShip(Number(numberOfPlayers));
         this.battleField = this.allBattleShip.reduce((prev, curr) => {
             prev = this.gameService.updateGrid(curr, this.battleField, curr.colorFront, curr.colorBack)
             return prev;
         }, this.battleField);
->>>>>>> f0745e1cee49e4405b95f97facfebe51ea0ff47a
+
 
         console.log('Start the game');
     }
